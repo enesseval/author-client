163cddfdb266fc7d3d07e62d5a321840d8be1b0c
enesseval <enesseval@outlook.com>
2025-04-08 21:22:01 +0300

    bug

--- middleware.ts
+++ middleware.ts
@@ -3,12 +3,10 @@ import { NextRequest, NextResponse } from "next/server";
 export const middleware = async (req: NextRequest) => {
    const token = req.cookies.get("accessToken");
 
-   console.log(token);
-
    const isAdminRoute = req.nextUrl.pathname.startsWith("/admin");
 
    if (isAdminRoute && !token) {
-      return NextResponse.redirect(new URL("/login", req.url));
+      return NextResponse.redirect(new URL("/dwaefsgrtrhf", req.url));
    }
 
    return NextResponse.next();
