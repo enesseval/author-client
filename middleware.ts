a5fc8fe20884147fc0d5d59e36a0f53e64ba2f07
enesseval <enesseval@outlook.com>
2025-04-09 19:39:11 +0300

    update middleware

--- middleware.ts
+++ middleware.ts
@@ -9,19 +9,7 @@ export const middleware = async (req: NextRequest) => {
 
    const isAdminRoute = req.nextUrl.pathname.startsWith("/admin");
 
-   // if (isAdminRoute) {
-   //    if (!tokenValue) {
-   //       console.log("Token not found, redirecting to /tekonyok"); // Yönlendirme nedenini logla
-   //       return NextResponse.redirect(new URL("/tekonyok", req.url));
-   //    } else {
-   //       // Token varsa /adminroute yerine doğrudan devam etmesini sağlayabilirsiniz
-   //       // VEYA gerçekten /adminroute diye bir sayfanız varsa oraya yönlendirebilirsiniz.
-   //       // Şimdilik sadece devam etmesini sağlıyorum:
-   //       console.log("Token found, proceeding.");
-   //       // return NextResponse.redirect(new URL("/adminroute", req.url)); // Eğer /adminroute varsa bu satırı kullanın
-   //       return NextResponse.next(); // Token varsa normal işleme devam et
-   //    }
-   // }
+   if (isAdminRoute && !token) NextResponse.redirect(new URL("/login", req.url));
 
    return NextResponse.next();
 };
