f4c2b2cf048d031f3fe95d7a5e3fc9dd43d89eea
enesseval <enesseval@outlook.com>
2025-04-09 18:56:55 +0300

    update next config

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,9 @@ const nextConfig = {
    eslint: {
       ignoreDuringBuilds: true, // Build sırasında ESLint hatalarını görmezden gel
    },
+   experimental: {
+      nodeMiddleware: true,
+   },
 };
 
 module.exports = nextConfig;
